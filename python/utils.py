5f3bdde4e04bb72e9564b0bc1eff22cbf64ad647
Marius <marius.bartcus@gmail.com>
2018-11-12 16:05:43 +0100

    MAP in utils

--- python/utils.py
+++ python/utils.py
@@ -179,7 +179,43 @@ def designmatrix_FRHLP(x,p,q=None):
     
     return phiBeta, phiW
             
-            
+
+
+def MAP(post_probas):
+    """
+     calcule une partition d'un echantillon par la regle du Maximum A Posteriori à partir des
+    
+     probabilites a posteriori 
+    
+     Entrees : post_probas , Matrice de dimensions [n x K] des probabibiltes a
+     posteriori (matrice de la partition floue)
+    
+           n : taille de l'echantillon
+    
+           K : nombres de classes
+    
+           klas(i) = arg   max (post_probas(i,k)) , for all i=1,...,n
+                         1<=k<=K
+                   = arg   max  p(zi=k|xi;theta)
+                         1<=k<=K
+                   = arg   max  p(zi=k;theta)p(xi|zi=k;theta)/sum{l=1}^{K}p(zi=l;theta) p(xi|zi=l;theta)
+                         1<=k<=K
+    
+     Sorties : classes : vecteur collones contenant les classe (1:K)
+    
+           Z : Matrice de dimension [nxK] de la partition dure : ses elements sont zik, avec zik=1 si xi
+           appartient à la classe k (au sens du MAP) et zero sinon.
+    
+    """
+    N, K = post_probas.shape
+    tmp,ikmax = post_probas.max(1)
+    partition_MAP = (ikmax@np.ones((0,K))) == (np.ones((N,1))@np.array([range(0,K)]));
+    klas = np.ones((N,1))
+    for k in range(0,K):
+        klas[partition_MAP[:,k]==1]=k
+    Z=partition_MAP
+    return klas, Z
+
 """
     ########################################
     start code for normalization of the data
